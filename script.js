ca2ae2ec67eca9e546d11418d7bc805a9759ee40
samirabaptista <86026341+samirabaptista@users.noreply.github.com>
2026-08-09 19:57:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector('.menu-button');
+const nav = document.querySelector('.nav');
+menuButton.addEventListener('click', () => {
+  const isOpen = nav.classList.toggle('open');
+  menuButton.setAttribute('aria-expanded', isOpen);
+});
+nav.querySelectorAll('a').forEach((link) => link.addEventListener('click', () => {
+  nav.classList.remove('open');
+  menuButton.setAttribute('aria-expanded', 'false');
+}));
